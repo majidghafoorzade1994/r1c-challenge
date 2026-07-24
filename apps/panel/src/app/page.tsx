ecb3432c2e95af09ff78d984d1b55fe482cd2d47
Majid Ghafoorzadeh <majidghafoorzade@gmail.com>
2026-07-24 17:35:50 +0330

    feat: implement panel app

---
+++ apps/panel/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="grid min-h-screen place-items-center">
+      <h1 className="text-2xl font-semibold">Blog Admin Panel</h1>
+    </main>
+  );
+}
